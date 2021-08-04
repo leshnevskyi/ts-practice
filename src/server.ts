3c9041c0abab9405e51db902d5530b3d4562a2e5
Leshnevskyi <leshnevskyi@gmail.com>
2021-08-04 19:12:11 +0300

    feat: response with an image if the request parameter matches a predefined key

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,30 @@
 import http from 'http';
+import fs from 'fs/promises';
 
-const requestListener: http.RequestListener = (_, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end(`<h1>Hey there!</h1>`);
+const BASE_URL = 'http://localhost:8000';
+
+const requestListener: http.RequestListener = async (req, res) => {
+  const key = new URL(req.url as string, BASE_URL).searchParams.get('key');
+
+  if (key === 'hey') {
+    const filePath = `${__dirname}/assets/images/iceland.jpg`;
+
+    try {
+      const stats = await fs.stat(filePath);
+      const image = await fs.readFile(filePath);
+
+      res.writeHead(200, {
+        'Content-Type': 'image/jpeg',
+        'Content-Length': stats.size,
+      });
+      res.end(image);
+    } catch(err) {
+      console.error(err);
+    }
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hey, try to tweak the search params!');
+  }
 }
 
 const PORT = 8000;
