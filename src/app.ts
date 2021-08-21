9c2058d064f1ff52f2289997dacb45f1f3bac9f6
Leshnevskyi <leshnevskyi@gmail.com>
2021-08-21 20:26:55 +0300

    feat: add a 'keypress' event handler

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+import readline from 'readline';
+
+readline.emitKeypressEvents(process.stdin);
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', () => {
@@ -7,3 +10,5 @@ process.stdin.on('readable', () => {
   process.stdout.write(input);  
   command === 'exit' && process.exit();
 });
+
+process.stdin.on('keypress', (_, key) => console.log(key));
